65e45a8dd6e30e2a10290fa1ca39f0746246c58e
Mark Harris <783069+harrism@users.noreply.github.com>
2023-06-06 15:42:56 +1000

    Add license and copyright

--- include/ranger/ranger.hpp
+++ include/ranger/ranger.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022-2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <cstdint>
 #include <iterator>
 
@@ -191,8 +206,8 @@ struct masked_step_range_iterator : public range_iterator<T> {
   }
 
  protected:
-  T _step;   // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
-  T _limit;  // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
+  T _step;                 // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
+  T _limit;                // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
   active_mask_type
     _initial_mask;         // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
   active_mask_type _mask;  // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
